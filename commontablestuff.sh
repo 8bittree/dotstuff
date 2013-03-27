749d0650cdf0f89f16ad6f67b95ba07937e65adc
8bittree <github.jao@jon.tomolin.net>
2013-03-27 18:17:43 -0400

    cleaned up commontablestuff.sh

--- commontablestuff.sh
+++ commontablestuff.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
 typeset -A dotlinks
-dotlinks=("$HOME/.vim" "$PWD/meat_and_potatoes/vim-config" \
-	"$HOME/.vimrc" "$HOME/.vim/vimrc" \
-	"$HOME/.zsh" "$PWD/meat_and_potatoes/zsh-config" \
-	"$HOME/.zshrc" "$HOME/.zsh/zshrc" \
-	"$HOME/.Xresources" "$PWD/meat_and_potatoes/Xresources")
+dotlinks=( \
+	"$HOME/.vim"		"$PWD/meat_and_potatoes/vim-config" \
+	"$HOME/.vimrc"		"$HOME/.vim/vimrc" \
+	"$HOME/.Xresources"	"$PWD/meat_and_potatoes/Xresources" \
+	"$HOME/.zsh"		"$PWD/meat_and_potatoes/zsh-config" \
+	"$HOME/.zshrc"		"$HOME/.zsh/zshrc" \
+	)
