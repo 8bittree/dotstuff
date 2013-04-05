bb34acbe73b98740ca78707849a6373f9dd4a373
8bittree <github.jao@jon.tomolin.net>
2013-04-05 17:20:00 -0400

    added zshenv to (un)installer scripts

--- commontablestuff.sh
+++ commontablestuff.sh
@@ -6,5 +6,6 @@ dotlinks=( \
 	"$HOME/.vimrc"		"$HOME/.vim/vimrc" \
 	"$HOME/.Xresources"	"$PWD/meat_and_potatoes/Xresources" \
 	"$HOME/.zsh"		"$PWD/meat_and_potatoes/zsh-config" \
+	"$HOME/.zshenv"		"$HOME/.zsh/zshenv" \
 	"$HOME/.zshrc"		"$HOME/.zsh/zshrc" \
 	)
