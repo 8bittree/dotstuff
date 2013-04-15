078077702e0dcf657c18fec8aa64138098086dd0
8bittree <github.jao@jon.tomolin.net>
2013-04-15 16:02:19 -0400

    added default herbstluftwm autostart

--- commontablestuff.sh
+++ commontablestuff.sh
@@ -2,10 +2,11 @@
 
 typeset -A dotlinks
 dotlinks=( \
-	"$HOME/.vim"		"$PWD/meat_and_potatoes/vim-config" \
-	"$HOME/.vimrc"		"$HOME/.vim/vimrc" \
-	"$HOME/.Xresources"	"$PWD/meat_and_potatoes/Xresources" \
-	"$HOME/.zsh"		"$PWD/meat_and_potatoes/zsh-config" \
-	"$HOME/.zshenv"		"$HOME/.zsh/zshenv" \
-	"$HOME/.zshrc"		"$HOME/.zsh/zshrc" \
+	"$HOME/.config/herbstluftwm"	"$PWD/meat_and_potatoes/herbstluftwm" \
+	"$HOME/.vim"					"$PWD/meat_and_potatoes/vim-config" \
+	"$HOME/.vimrc"					"$HOME/.vim/vimrc" \
+	"$HOME/.Xresources"				"$PWD/meat_and_potatoes/Xresources" \
+	"$HOME/.zsh"					"$PWD/meat_and_potatoes/zsh-config" \
+	"$HOME/.zshenv"					"$HOME/.zsh/zshenv" \
+	"$HOME/.zshrc"					"$HOME/.zsh/zshrc" \
 	)
